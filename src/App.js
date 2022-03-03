3f2d92dc21bad7c6ff839826564d9b3af38c864a
hassanidris <hassan.creative@gmail.com>
2022-03-03 15:51:26 +0300

    Uploading first React App

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+import Navbar from './Navbar';
+import Hero from './Hero';
+
+function App() {
+  return (
+    <header className="header">
+      <Navbar />
+      <Hero />
+    </header>
+  );
+}
+
+export default App;
+
+
+// import React from 'react';
+// // import logo from './logo.svg';
+// // import './App.css';
+// import Header from './Header';
+
+// function App() {
+//   return (
+//     <Header />
+//   );
+// }
+
+// export default App;
---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+import Navbar from './Navbar';
+import Hero from './Hero';
+
+function Header() {
+  return (
+    <Header>
+    <Navbar />
+    <Hero />
+    </Header>
+  );
+}
+
+export default Header;
